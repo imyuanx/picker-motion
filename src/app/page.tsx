90628dc60850336fbe72ef444d7d98017f9eb88a
yuanx <yuanx.me@qq.com>
2023-10-31 23:00:27 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,133 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { motion, useAnimate } from "framer-motion";
+import clsx from "clsx";
+
+const containerWidth = 256;
+const itemWidth = 80;
+const containerCenter = containerWidth / 2 - itemWidth / 2;
+const position = {
+  init: {
+    L: {
+      left: containerCenter - 30,
+      top: -20,
+      rotate: -16,
+      scale: 0.75,
+    },
+    M: {
+      left: containerCenter,
+      top: 0,
+      rotate: 0,
+      scale: 1,
+    },
+    R: {
+      left: containerCenter + 30,
+      top: -20,
+      rotate: 16,
+      scale: 0.75,
+    },
+  },
+  hover: {
+    L: {
+      left: 0,
+      top: 0,
+      rotate: 0,
+      scale: 0.8,
+    },
+    M: {
+      left: containerCenter,
+      top: 0,
+      rotate: 0,
+      scale: 0.8,
+    },
+    R: {
+      left: containerWidth - itemWidth,
+      top: 0,
+      rotate: 0,
+      scale: 0.8,
+    },
+  },
+};
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+  const [current, setCurrent] = useState(1);
+  const containerRef = useRef(null);
+
+  const [scopeA, animateA] = useAnimate();
+  const [scopeB, animateB] = useAnimate();
+  const [scopeC, animateC] = useAnimate();
+
+  const ITEM_LIST = [
+    { id: 0, name: "A", scope: scopeA, animate: animateA },
+    { id: 1, name: "B", scope: scopeB, animate: animateB },
+    { id: 2, name: "C", scope: scopeC, animate: animateC },
+  ];
+
+  function isLeft(id: number) {
+    return (
+      Math.min(
+        ...ITEM_LIST.filter(({ id }) => id !== current).map(({ id }) => id)
+      ) === id
+    );
+  }
+
+  function onHoverStart() {
+    animation("hover");
+  }
+  function onHoverEnd() {
+    animation("init");
+  }
+
+  function animation(state: "init" | "hover") {
+    ITEM_LIST.map(({ id, scope, animate }) => {
+      const flag = id === current ? "M" : isLeft(id) ? "L" : "R";
+      animate(scope.current, position[state][flag]);
+    });
+  }
+
+  function onReset() {
+    setCurrent(1);
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    animation("init");
+  }, [count, current]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    <main className="flex flex-col gap-28 min-h-screen justify-center items-center">
+      <motion.div
+        ref={containerRef}
+        className="flex items-center relative w-64 h-20"
+        key={count}
+        onHoverStart={onHoverStart}
+        onHoverEnd={onHoverEnd}
+      >
+        {ITEM_LIST.map(({ id, scope }) => (
+          <motion.div
+            ref={scope}
+            className={clsx(
+              "absolute left-[88px] scale-95 w-20 h-20 border-2 border-gray-500 bg-white rounded-xl duration-100 hover:!scale-90 cursor-pointer",
+              id === current && "z-10 bg-gray-100"
+            )}
+            onClick={() => setCurrent(id)}
+          />
+        ))}
+      </motion.div>
+      <button className="underline" onClick={onReset}>
+        Reset
+      </button>
+      <p className="fixed bottom-0 p-4 w-full text-gray-400">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://twitter.com/imyuanx"
           target="_blank"
-          rel="noopener noreferrer"
+          className="underline"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+          @yuanx
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </p>
     </main>
-  )
+  );
 }
